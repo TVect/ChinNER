59809153ac7ed488226d3bced32244f83b458363
TVect <chin340823@163.com>
2019-06-12 18:14:28 +0800

    update evaluate.py

--- utils/evaluate.py
+++ utils/evaluate.py
@@ -1,11 +1,10 @@
 import os
 from utils.conlleval import return_report
 
-def test_ner(results, path):
+def test_ner(results, output_file):
     """
     Run perl script to evaluate model
     """
-    output_file = os.path.join(path, "ner_predict.utf8")
     with open(output_file, "w") as f:
         to_write = []
         for block in results:
@@ -16,3 +15,17 @@ def test_ner(results, path):
         f.writelines(to_write)
     eval_lines = return_report(output_file)
     return eval_lines
+
+
+def evaluate_with_conlleval(texts, golds, preds, output_file):
+    assert len(texts) == len(golds) == len(preds), \
+           "len(texts):{}, len(golds):{}, len(preds):{}".format(len(texts), len(golds), len(preds))
+    results = []
+    for text, gold, pred in zip(texts, golds, preds):
+        assert len(text) == len(gold) == len(pred), \
+               "len(text):{}, len(gold):{}, len(pred):{}".format(len(text), len(gold), len(pred))
+        result = [" ".join([char_item, gold_item, pred_item]) 
+                  for char_item, gold_item, pred_item in zip(text, gold, pred)]
+        results.append(result)
+    return test_ner(results, output_file)
+
