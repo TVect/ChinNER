c2fce56803551427d3b55515c1bb1be6bbb7a8dc
TVect <chin340823@163.com>
2019-06-22 19:57:56 +0800

    update run_bert.sh

--- bin/run_bert.sh
+++ bin/run_bert.sh
@@ -4,13 +4,9 @@ cd ..
 
 python -m models.bert_ner.main \
 	--do_lower_case=False \
-    --do_train=True   \
-    --do_eval=True   \
-    --do_predict=True \
-    --data_dir=data   \
     --use_crf=False    \
     --max_seq_length=128   \
-    --train_batch_size=32   \
+    --batch_size=32   \
     --learning_rate=2e-5   \
-    --num_train_epochs=4.0   \
+    --num_train_epochs=4   \
     --output_dir=./models/bert_ner/output
